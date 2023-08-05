05a194a978f58155318584e21176791155b002dc
Shirakawa42 <Shirakawa@live.fr>
2023-08-05 17:15:38 +0200

    basic collisions

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -10,8 +10,29 @@ public static class Globals
 
     public static GameObject Player;
 
-    public static Vector2Int GetChunkPositionFromWorldPosition(Vector2Int worldPosition)
+    public static Vector2Int GetChunkPositionFromWorldPosition(Vector3 worldPosition)
     {
-        return new Vector2Int(worldPosition.x / ChunkSize, worldPosition.y / ChunkSize);
+        return new Vector2Int(Mathf.FloorToInt(worldPosition.x / ChunkSize), Mathf.FloorToInt(worldPosition.y / ChunkSize));
+    }
+
+    public static Vector2Int GetBlockPositionFromWorldPosition(Vector3 worldPosition)
+    {
+        int blockPosX = Mathf.FloorToInt(worldPosition.x);
+        int blockPosY = Mathf.FloorToInt(worldPosition.y);
+
+        blockPosX = (blockPosX % ChunkSize + ChunkSize) % ChunkSize;
+        blockPosY = (blockPosY % ChunkSize + ChunkSize) % ChunkSize;
+
+        return new Vector2Int(blockPosX, blockPosY);
+    }
+
+    public static BlockStats GetSolidBlockStatsFromWorldPosition(Vector3 worldPosition)
+    {
+        Vector2Int chunkPosition = GetChunkPositionFromWorldPosition(worldPosition);
+        Vector2Int blockPosition = GetBlockPositionFromWorldPosition(worldPosition);
+
+        if (Map.SolidChunks.ContainsKey(chunkPosition))
+            return AllBlocksStats.GetBlockStatsFromId(Map.SolidChunks[chunkPosition].Blocs[blockPosition.x + blockPosition.y * ChunkSize].Id);
+        return AllBlocksStats.GetBlockStatsFromId(BlockIds.Air);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 8f;
+    public float PlayerSize;
+
+    void Start()
+    {
+        Transform playerSkin = transform.Find("PlayerSkin");
+        PlayerSize = playerSkin.localScale.x;
+    }
 
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        Vector3 movement = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         if (movement.magnitude > 1.0f)
         {
             movement.Normalize();
         }
-        transform.position += Speed * Time.deltaTime * movement;
+
+        Vector3 futurePositionH = transform.position + new Vector3(Speed * Time.deltaTime * movement.x, 0f, 0f);
+        Vector3 futurePositionV = transform.position + new Vector3(0f, Speed * Time.deltaTime * movement.y, 0f);
+
+        Vector3 moveH = new(Speed * Time.deltaTime * movement.x, 0f, 0f);
+        Vector3 moveV = new(0f, Speed * Time.deltaTime * movement.y, 0f);
+        Vector3 move = new(0f, 0f, 0f);
+
+        // Collision check for horizontal movement
+        Vector3 topLeftH = new(futurePositionH.x - PlayerSize / 2, futurePositionH.y + PlayerSize / 2, futurePositionH.z);
+        Vector3 topRightH = new(futurePositionH.x + PlayerSize / 2, futurePositionH.y + PlayerSize / 2, futurePositionH.z);
+        Vector3 bottomLeftH = new(futurePositionH.x - PlayerSize / 2, futurePositionH.y - PlayerSize / 2, futurePositionH.z);
+        Vector3 bottomRightH = new(futurePositionH.x + PlayerSize / 2, futurePositionH.y - PlayerSize / 2, futurePositionH.z);
+
+        if (Globals.GetSolidBlockStatsFromWorldPosition(topLeftH).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(topRightH).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(bottomLeftH).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(bottomRightH).IsSolid == false)
+        {
+            move += moveH;
+        }
+
+        // Collision check for vertical movement
+        Vector3 topLeftV = new(futurePositionV.x - PlayerSize / 2, futurePositionV.y + PlayerSize / 2, futurePositionV.z);
+        Vector3 topRightV = new(futurePositionV.x + PlayerSize / 2, futurePositionV.y + PlayerSize / 2, futurePositionV.z);
+        Vector3 bottomLeftV = new(futurePositionV.x - PlayerSize / 2, futurePositionV.y - PlayerSize / 2, futurePositionV.z);
+        Vector3 bottomRightV = new(futurePositionV.x + PlayerSize / 2, futurePositionV.y - PlayerSize / 2, futurePositionV.z);
+
+        if (Globals.GetSolidBlockStatsFromWorldPosition(topLeftV).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(topRightV).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(bottomLeftV).IsSolid == false &&
+            Globals.GetSolidBlockStatsFromWorldPosition(bottomRightV).IsSolid == false)
+        {
+            move += moveV;
+        }
+
+        // Apply the effective move
+        transform.position += move;
     }
 }
