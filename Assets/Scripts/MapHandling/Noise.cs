590fc5a880fc33860bcfcd2c2efe81bcb5e39407
Shirakawa42 <Shirakawa@live.fr>
2023-08-11 17:16:28 +0200

    stone has returned, trees are spawning more normally

--- Assets/Scripts/MapHandling/Noise.cs
+++ Assets/Scripts/MapHandling/Noise.cs
@@ -15,10 +15,35 @@ public static class Noise
 
     public static ushort GetSolidBlockAtWorldPosition(Vector2Int position, WorldsIds worldId)
     {
+        if (worldId == WorldsIds.overworld)
+            return OverWorldGenerator(position);
+        else
+            return ItemIds.Air;
+    }
+
+    private static ushort OverWorldGenerator(Vector2Int position)
+    {
         float noise = Mathf.PerlinNoise(position.x / 5f + Globals.Seed, position.y / 5f + Globals.Seed);
         if (noise < 0.5f)
-            return ItemIds.Air;
+            return ForestGenerator(position);
         else
-            return ItemIds.Wood;
+            return ItemIds.Stone;
+    }
+
+    private static ushort ForestGenerator(Vector2Int position)
+    {
+        int treeSpacing = 3;
+        Vector2Int samplePosition = new(Mathf.FloorToInt(position.x / treeSpacing) * treeSpacing,
+                                                   Mathf.FloorToInt(position.y / treeSpacing) * treeSpacing);
+
+        if (Mathf.Abs(position.x - samplePosition.x) <= 1 && Mathf.Abs(position.y - samplePosition.y) <= 1)
+        {
+            float treeNoise = Mathf.PerlinNoise((position.x / 2f) + (Globals.Seed * 3), (position.y / 2f) + (Globals.Seed * 3));
+
+            if (treeNoise > 0.8f)
+                return ItemIds.Wood;
+        }
+
+        return ItemIds.Air;
     }
 }
